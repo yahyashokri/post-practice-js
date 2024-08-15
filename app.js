15718ae105c1b29db8032c1b3f30830493b14286
yahyashokri <yahyashokri24@gmail.com>
2024-08-15 16:11:40 +0330

    final

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 const container = document.getElementById('container')
 
 async function callPosts(e) {
+    e.preventDefault()
     try{
         const response = await fetch("https://jsonplaceholder.typicode.com/posts" , { method : 'GET'})
         const posts = await response.json()
         posts.forEach(function (post,i){
             const tag = document.createElement('div')
-            tag.setAttribute(id,`tag${i}`)
-            let keys = Object.keys(post)
-            let values = Object.values(post)
-            const cont =document.createElement("div")
-            cont.setAttribute(id,`cont${i}`)
-            tag.appendChild(cont)
+            tag.setAttribute('id',`tag${i}`)
             tag.innerHTML =`
-    <h2>post${i + 1}</h2> 
-    `;
-            keys.forEach(function (key,i2) {
-                const value = values[i2]
-                let pTag = document.createElement('p')
-                pTag.innerText = `${key} : ${value}`;
-                document.getElementById(`cont${i}`).appendChild(pTag)
-               });
+            <h2>post${i + 1}</h2> 
+            <p>userID : ${post.userId}</p>
+            <p>ID : ${post.id}</p>
+            <p>Title : ${post.title}</p>
+            <p>Body : ${post.body}</p>
+            `;
+    console.log(tag)
     container.appendChild(tag)
     });
     }
